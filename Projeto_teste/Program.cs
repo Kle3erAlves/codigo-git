68cde030085f4982c9e1adc8c446e9e714974baa
kleber G <kleber@marcamp.com.br>
2021-08-09 23:32:14 -0300

    projeto iniciado ola mundo bom dia

---
+++ Projeto_teste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Bom dia!");
+
+            Console.ReadLine();
+        }
+    }
+}
